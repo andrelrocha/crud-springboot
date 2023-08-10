15e90ea093aea2742f3f9c1d193151a154e802ba
andrelrocha <andrerocha0911@gmail.com>
2023-08-10 12:32:36 -0300

    feat: msg de erro personalizada dos campos inválidos em requisições 400

--- src/main/java/rocha/andre/api/infra/ErrorHandling.java
+++ src/main/java/rocha/andre/api/infra/ErrorHandling.java
@@ -2,19 +2,35 @@ package rocha.andre.api.infra;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.security.PublicKey;
 
 @RestControllerAdvice
 public class ErrorHandling {
 
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity handleEntityNotFoundException(EntityNotFoundException ex) {
+    public ResponseEntity handleEntityNotFoundException() {
         return ResponseEntity.notFound().build();
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity handleIllegalArgumentException(IllegalArgumentException ex) {
         return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        var errors = ex.getFieldErrors();
+        return ResponseEntity.badRequest().body(errors.stream().map(DataValidationError::new).toList());
+    }
+
+    private record DataValidationError(String field, String message) {
+        public DataValidationError(FieldError error) {
+            this(error.getField(), error.getDefaultMessage());
+        }
     }
 }
