8e8eea8d8e36c9d647a6e02745f2808d2d102af9
andrelrocha <andrerocha0911@gmail.com>
2023-08-22 09:03:36 -0300

    feat: migration creating user table

---
+++ src/main/java/rocha/andre/api/domain/user/User.java
@@ -0,0 +1,21 @@
+package rocha.andre.api.domain.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "users")
+@Entity(name = "User")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String login;
+    private String password;
+}
