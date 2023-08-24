9b79f58fc0254dd09ef61bfef4d069fa24e13489
andrelrocha <andrerocha0911@gmail.com>
2023-08-24 11:02:27 -0300

    refactor: att securityfilterchain removendo partes depreciated

--- src/main/java/rocha/andre/api/infra/security/SecurityConfigurations.java
+++ src/main/java/rocha/andre/api/infra/security/SecurityConfigurations.java
@@ -23,12 +23,13 @@ public class SecurityConfigurations {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http.csrf().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and().authorizeHttpRequests()
-                .requestMatchers(HttpMethod.POST, "/login").permitAll()
-                .anyRequest().authenticated()
-                .and().addFilterBefore(securityFilterApplication, UsernamePasswordAuthenticationFilter.class)
+        return http.csrf(csrf -> csrf.disable())
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(req -> {
+                    req.requestMatchers(HttpMethod.POST, "/login").permitAll();
+                    req.anyRequest().authenticated();
+                })
+                .addFilterBefore(securityFilterApplication, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
