32185d60183265182d76310d9ddd6e686a6b77d4
andrelrocha <andrerocha0911@gmail.com>
2023-08-24 10:58:12 -0300

    feat: autenticando o user e controlando o acesso à aplicação c filter próprio

--- src/main/java/rocha/andre/api/infra/security/SecurityFilter.java
+++ src/main/java/rocha/andre/api/infra/security/SecurityFilter.java
@@ -5,8 +5,11 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import rocha.andre.api.domain.user.User;
 
 import java.io.IOException;
 
@@ -15,23 +18,31 @@ public class SecurityFilter extends OncePerRequestFilter {
 
     @Autowired
     private TokenService tokenService;
+    @Autowired
+    AuthenticateUserWithValidJwt authenticateUserWithValidJwt;
+
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String tokenJwt = getToken(request);
 
-        String subject = tokenService.getSubject(tokenJwt);
-
+        if (tokenJwt != null) {
+            String subject = tokenService.getSubject(tokenJwt);
+            User user = authenticateUserWithValidJwt.findUserAuthenticated(subject);
+            //avisa para o spring que o usuário está autenticado
+            var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
 
         filterChain.doFilter(request, response);
     }
 
     private String getToken(HttpServletRequest request) {
         String authorizationHeader = request.getHeader("Authorization");
-        if (authorizationHeader == null) {
-            throw new RuntimeException("Token JWT not found in the request header");
+        if (authorizationHeader != null) {
+            return authorizationHeader.replace("Bearer ", "");
         }
 
-        return authorizationHeader.replace("Bearer ", "");
+        return null;
     }
 }
